c6f375915ca1a73cec61c1eb319160c87257ff6e
FelineJTD <13520050@std.stei.itb.ac.id>
2023-10-25 01:51:46 +0200

    chore: add color style

--- quasar-project/tailwind.config.js
+++ quasar-project/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white':'#FFFFFF',
+        'lnf-navy':'#233977',
+        'lnf-red':'#EA0029',
+        'lnf-orange':'#E65300',
+      }
+    },
   },
   plugins: [],
 }
