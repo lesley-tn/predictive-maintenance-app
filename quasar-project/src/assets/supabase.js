80e01c08e96f02e453a4f1737cfe69a19b950e69
lesley-tn <lesley.znh@gmail.com>
2023-10-24 22:55:48 +0200

    showing client/building info with the button

--- quasar-project/src/assets/supabase.js
+++ quasar-project/src/assets/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ajtbgaoittxdpfvqkniu.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqdGJnYW9pdHR4ZHBmdnFrbml1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY1OTk2NTcsImV4cCI6MjAxMjE3NTY1N30.0xqLfRIcjMTxgQPw_ElgdjhQWlba5kKMRgQL-tcha14';
--- quasar-project/src/pages/Home.vue
+++ quasar-project/src/pages/Home.vue
@@ -15,19 +15,40 @@
       </div>
     </div>
 
-      <!-- Display client names in a q-card section in a 2x5 grid -->
+ <!-- toggle button for client and buildings -->
+    <div class="q-pa-md">
+      <q-btn-toggle
+        v-model="secondModel"
+        spread
+        class="clients-buildings-toggle"
+        no-caps
+        rounded
+        unelevated
+        toggle-color="red"
+        
+        text-color="black"
+        :options="[
+          {label: 'Clients', value: 'one'},
+          {label: 'Buildings', value: 'two'}
+        ]"
+      />
+    </div>
+
+
+
+      <!-- Display client or building names in q-card section in a 2x5 grid -->
       <div class="q-pa-md">
-      <h3>Clients</h3>
+      
       <div class="row q-gutter-md">
         <!-- Loop to generate 10 q-card elements -->
         <div
           class="col-2 q-mb-md"
-          v-for="index in 10"
+          v-for="(info, index) in (secondModel === 'one' ? clients : buildings)"
           :key="index"
         >
-          <q-card>
-            <q-card-section v-if="index <= Clients_name.length">
-              Client Name: {{ Clients_name[index - 1] }}
+          <q-card @click="showInfoDialog(info)">
+            <q-card-section v-if="info">
+              {{ secondModel === 'one' ? info.Clients_name : info.Building_name }}
             </q-card-section>
             <q-card-section v-else>
               <!-- Empty card -->
@@ -36,21 +57,31 @@
         </div>
       </div>
     </div>
+
+    <!-- Dialog to show client or building information -->
+    <q-dialog v-model="infoDialog">
+      <q-card>
+        <q-card-section>
+          <h4>
+            {{ secondModel === 'one' ? selectedInfo.Clients_name : selectedInfo.Building_name }}
+          </h4>
+          <p v-if="selectedInfo">
+            {{ secondModel === 'one' ? 'Client ID' : 'Building ID' }}: {{ secondModel === 'one' ? selectedInfo.Clients_id : selectedInfo.Buildings_id }}
+          </p>
+          <p v-if="selectedInfo">Phone Number: {{ selectedInfo.Phone_number }}</p>
+          <p v-if="selectedInfo">
+            {{ secondModel === 'one' ? 'Address' : 'Building Address' }}: {{ formatAddress(selectedInfo) }}
+          </p>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn label="Close" color="primary" @click="closeInfoDialog" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
+ 
 
-<style lang="sass">
-.overview-row
-  .row > div
-    padding: 15px 15px
-    margin-right: 20px
-    background: rgba(#999, .15)
-    border: 1px solid rgba(#999, .2)
-  .row + .row
-    margin-top: 1rem
-
-
-</style>
 
 <script>
 import { ref, onMounted } from 'vue';
@@ -63,28 +94,48 @@ export default {
     const totalBuildings = ref(0);
     const totalProjects = ref(0);
     const Clients_name = ref([]);
-    const clientIds = ref([]);
+    const clients = ref([]);
+    const buildings = ref([]);
+    const infoDialog = ref(false);
+    const selectedInfo = ref(null);
+    const secondModel = ref('one'); // 'one' for Clients, 'two' for Buildings
 
     const fetchData = async () => {
-      const { data: clients, error: clientsError } = await supabase
+      const { data: clientsData, error: clientsError } = await supabase
         .from('Clients')
-        .select('Clients_id, Clients_name');
+        .select('*');
 
-      const { data: buildings, error: buildingsError } = await supabase
+      const { data: buildingsData, error: buildingsError } = await supabase
         .from('Buildings')
-        .select('Buildings_id');
+        .select('*');
 
       const { data: projects, error: projectsError } = await supabase
         .from('MaintenanceProjects')
         .select('Project_id');
 
       if (!clientsError) {
-        totalClients.value = clients.length;
-        Clients_name.value = clients.map((client) => client.Clients_name);
-        clientIds.value = clients.map((client) => client.Clients_id);
+        totalClients.value = clientsData.length;
+        Clients_name.value = clientsData.map((client) => client.Clients_name);
+        clients.value = clientsData;
       }
-      if (!buildingsError) totalBuildings.value = buildings.length;
+      if (!buildingsError) {
+        totalBuildings.value = buildingsData.length;
+        buildings.value = buildingsData;
+      }
       if (!projectsError) totalProjects.value = projects.length;
+    };
+
+    const showInfoDialog = (info) => {
+      selectedInfo.value = info;
+      infoDialog.value = true;
+    };
+
+    const closeInfoDialog = () => {
+      infoDialog.value = false;
+    };
+
+    const formatAddress = (building) => {
+      return `${building.Building_street} ${building.Building_streetNumber}, ${building.Building_zipCode}, ${building.Building_city}`;
     };
 
     onMounted(() => {
@@ -96,8 +147,29 @@ export default {
       totalBuildings,
       totalProjects,
       Clients_name,
-      clientIds,
+      clients,
+      buildings,
+      showInfoDialog,
+      closeInfoDialog,
+      infoDialog,
+      selectedInfo,
+      secondModel,
+      formatAddress,
     };
   },
 };
 </script>
+
+<style lang="sass">
+.overview-row
+  .row > div
+    padding: 15px 15px
+    margin-right: 20px
+    background: rgba(#999, .15)
+    border: 1px solid rgba(#999, .2)
+  .row + .row
+    margin-top: 1rem
+
+  .clients-buildings-toggle
+    border: 1px solid black
+</style>
