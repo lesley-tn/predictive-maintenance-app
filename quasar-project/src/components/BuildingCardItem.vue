286cc0c397675a02f772118cbeef4530f4437890
FelineJTD <13520050@std.stei.itb.ac.id>
2023-10-28 18:34:51 +0200

    feat: add lenferink logo

--- quasar-project/src/components/BuildingCardItem.vue
+++ quasar-project/src/components/BuildingCardItem.vue
@@ -1,101 +1,85 @@
 <template>
-    <!-- loop to generate all infos, filter by search -->
-    <div class="q-pa-md q-gutter-sm">
+  <!-- loop to generate all infos, filter by search -->
+  <div>
     <button
-           class="border border-lnf-navy rounded-xl px-5 py-4 hover:bg-lnf-navy/5 duration-100 text-left"
-     
-           @click="infoDialog = true"
-         >
-           <p>
-             {{ item.Building_name }}
-           </p>
-           
-   </button>
- 
- 
-         <q-dialog v-model="infoDialog">
-     
-       <q-card>
-             <q-card-section class="q-pa-md">
-               <h4>
-                 {{item.Building_name }}
-               </h4>
-               <p >
-                 Building ID: {{ item.Buildings_id  }}
-               </p>
-     
-               <p >
-                Client ID: {{ item.Clients_id }}
-               </p>
+      class="bg-lnf-navy/5 rounded-xl px-5 py-4 text-left w-full"
+      @click="infoDialog = true"
+    >
+      <p>
+        {{ item.Building_name }}
+      </p>
+    </button>
 
-               <p >
-                Building Address: {{ fullBuildingAddress }}
-               </p>
-     
-             
-             </q-card-section>
-             <!-- Buildings button in client information-->
-             <q-card-actions class="q-pa-md">
-               <q-btn
-               label="Buildings"
-               v-on:click="go_to_client_buildings(selectedInfo.Clients_name)"  
-               v-if="secondModel === 'one'"
-               style="text-transform: none;"
-             />
-             </q-card-actions>
-     
-             <q-card-actions align="right">
-               <q-btn label="Close" color="primary" @click="infoDialog = false" />
-             </q-card-actions>
-           </q-card>
-         </q-dialog>
-          
-           
-     </div>
- </template>
-     
-   <script>
-     import { ref } from 'vue';
-     export default {
-       name: 'BuildingCardItem',
-       props: ['item'],
- 
-       methods:{
-         OpenDialogBox(){
-           
-           console.log('REFRESGIN')
-         },
-         
-     },
- 
-         setup(props) {
-            const fullBuildingAddress = props.item.Building_street + " " + props.item.Building_streetNumber + ", " +  props.item.Building_zipCode + ", " + props.item.Building_city
-             const infoDialog = ref(false);
-             
-             
-                 
- 
-             return {
-            
-             infoDialog,
-             fullBuildingAddress,
- 
-         };
-         }
- 
-     
-     };
-   </script>
-     
- <!-- 
-     ? 
+    <q-dialog v-model="infoDialog">
+      <q-card>
+        <q-card-section class="q-pa-md">
+          <h4>
+            {{ item.Building_name }}
+          </h4>
+          <p>Building ID: {{ item.Buildings_id }}</p>
+
+          <p>Client ID: {{ item.Clients_id }}</p>
+
+          <p>Building Address: {{ fullBuildingAddress }}</p>
+        </q-card-section>
+        <!-- Buildings button in client information-->
+        <q-card-actions class="q-pa-md">
+          <q-btn
+            label="Buildings"
+            v-on:click="go_to_client_buildings(selectedInfo.Clients_name)"
+            v-if="secondModel === 'one'"
+            style="text-transform: none"
+          />
+        </q-card-actions>
+
+        <q-card-actions align="right">
+          <q-btn label="Close" color="primary" @click="infoDialog = false" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  name: 'BuildingCardItem',
+  props: ['item'],
+
+  methods: {
+    OpenDialogBox() {
+      console.log('REFRESGIN');
+    },
+  },
+
+  setup(props) {
+    const fullBuildingAddress =
+      props.item.Building_street +
+      ' ' +
+      props.item.Building_streetNumber +
+      ', ' +
+      props.item.Building_zipCode +
+      ', ' +
+      props.item.Building_city;
+    const infoDialog = ref(false);
+
+    return {
+      infoDialog,
+      fullBuildingAddress,
+    };
+  },
+};
+</script>
+
+<!--
+     ?
      // IF CLIENTS SELECTED
      clients.filter((client) => client.Clients_name.toLowerCase().includes(search.toLowerCase()))
-     
+
      // .filter((buiilding) => buildingsData.Clients_id.includes(s))
-     
-     : 
+
+     :
      // IF BUILDINGS SELECTED
      buildings.filter((building) => building.Building_name.toLowerCase().includes(search.toLowerCase()))
-     
+
      " -->
--- quasar-project/src/components/ClientCardItem.vue
+++ quasar-project/src/components/ClientCardItem.vue
@@ -1,115 +1,99 @@
 <template>
-   <!-- loop to generate all infos, filter by search -->
-   <div class="q-pa-md q-gutter-sm">
-   <button
-          class="border border-lnf-navy rounded-xl px-5 py-4 hover:bg-lnf-navy/5 duration-100 text-left"
-    
-          @click="infoDialog = true"
-        >
-          <p>
-            {{ item.Clients_name }}
-          </p>
-          
-  </button>
-
+  <!-- loop to generate all infos, filter by search -->
+  <div>
+    <button
+      class="bg-lnf-navy/5 rounded-xl px-5 py-4 text-left w-full"
+      @click="infoDialog = true"
+    >
+      <p>
+        {{ item.Clients_name }}
+      </p>
+    </button>
 
     <q-dialog v-model="infoDialog">
-    
       <q-card>
         <q-card-section>
           <h4>
-            {{item.Clients_name }}
-
-             
+            {{ item.Clients_name }}
           </h4>
           <q-btn
-              v-if="!editing"
-              @click="startEditing"
-              icon="edit"
-              class="edit-button"
-              />
-          <p >
-            Client ID: {{ item.Clients_id  }}
-          </p>
-    
-          <p >
-            Phone number: {{ item.Phone_number }}
-          </p>
-    
-          <p >
-            Address: {{ fullAddress }}
-          </p>
-          
+            v-if="!editing"
+            @click="startEditing"
+            icon="edit"
+            class="edit-button"
+          />
+          <p>Client ID: {{ item.Clients_id }}</p>
+
+          <p>Phone number: {{ item.Phone_number }}</p>
+
+          <p>Address: {{ fullAddress }}</p>
         </q-card-section>
-            <!-- Buildings button in client information-->
-          <q-card-actions class="q-pa-md">
-            <q-btn
-              label="Buildings"
-              v-on:click="go_to_client_buildings()"  
-     
-              style="text-transform: none;"
-            />
-          </q-card-actions>
-    
-          <q-card-actions align="right">
-              <q-btn label="Close" color="primary" @click="infoDialog = false" />
-            </q-card-actions>
-          </q-card>
-        </q-dialog>
-         
-          
-    </div>
+        <!-- Buildings button in client information-->
+        <q-card-actions class="q-pa-md">
+          <q-btn
+            label="Buildings"
+            v-on:click="go_to_client_buildings()"
+            style="text-transform: none"
+          />
+        </q-card-actions>
+
+        <q-card-actions align="right">
+          <q-btn label="Close" color="primary" @click="infoDialog = false" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
 </template>
-    
-  <script>
-    import { ref } from 'vue';
-    export default {
-      name: 'ClientCardItem',
-      props: ['item'],
 
-      methods:{
-        go_to_client_buildings(){
-          this.$emit('exit_card', this.props.item);
-          console.log('REFRESGIN')
-        },
+<script>
+import { ref } from 'vue';
+export default {
+  name: 'ClientCardItem',
+  props: ['item'],
+
+  methods: {
+    go_to_client_buildings() {
+      this.$emit('exit_card', this.props.item);
+      console.log('REFRESGIN');
     },
+  },
 
-        setup(props) {
-          const fullAddress = props.item.Address_StreetName + " " + props.item.Address_HouseNumber + ", " +  props.item.Address_Zipcode + ", " + props.item.Address_City
+  setup(props) {
+    const fullAddress =
+      props.item.Address_StreetName +
+      ' ' +
+      props.item.Address_HouseNumber +
+      ', ' +
+      props.item.Address_Zipcode +
+      ', ' +
+      props.item.Address_City;
 
-            const infoDialog = ref(false);
-            
-                
+    const infoDialog = ref(false);
 
-            return {
-            props,
-            infoDialog,
-            fullAddress,
-
-        };
-        }
-
-    
+    return {
+      props,
+      infoDialog,
+      fullAddress,
     };
-  </script>
-    
-<!-- 
-    ? 
+  },
+};
+</script>
+
+<!--
+    ?
     // IF CLIENTS SELECTED
     clients.filter((client) => client.Clients_name.toLowerCase().includes(search.toLowerCase()))
-    
+
     // .filter((buiilding) => buildingsData.Clients_id.includes(s))
-    
-    : 
+
+    :
     // IF BUILDINGS SELECTED
     buildings.filter((building) => building.Building_name.toLowerCase().includes(search.toLowerCase()))
-    
+
     " -->
 
-
-    <style scoped>
+<style scoped>
 .edit-button {
   margin-left: auto; /* Push the button to the right side */
 }
 </style>
-    
