803f849c2060de87588045c66615525651db21f6
FelineJTD <13520050@std.stei.itb.ac.id>
2023-11-01 11:10:54 +0100

    fix: client and building card

--- quasar-project/src/components/BuildingCardItem.vue
+++ quasar-project/src/components/BuildingCardItem.vue
@@ -2,166 +2,158 @@
   <!-- loop to generate all infos, filter by search -->
   <div>
     <button
-           class="border border-lnf-navy rounded-xl px-5 py-4 hover:bg-lnf-navy/5 duration-100 text-left"
-     
-           @click="infoDialog = true"
-         >
-           <p>
-             {{ item.Building_name }}
-           </p>
-           
-   </button>
- 
- 
- <q-dialog v-model="infoDialog" @hide="editing=false">
-     
-  <q-card>
-    <q-card-section class="q-pa-md">
-      <template v-if="!editing">
-          <div class="row justify-between">
-            <h4>
-              {{ item.Building_name }}
-            </h4>
-            <q-btn
-              v-if="!editing"
-              @click="startEditing"
-              icon="edit"
+      class="bg-lnf-navy/5 rounded-xl px-5 py-4 text-left w-full"
+      @click="infoDialog = true"
+    >
+      <p>
+        {{ item.Building_name }}
+      </p>
+    </button>
+
+    <q-dialog v-model="infoDialog" @hide="editing = false">
+      <q-card>
+        <q-card-section class="q-pa-md">
+          <template v-if="!editing">
+            <div class="row justify-between">
+              <h4>
+                {{ item.Building_name }}
+              </h4>
+              <q-btn v-if="!editing" @click="startEditing" icon="edit" />
+            </div>
+
+            <p>Building ID: {{ item.Buildings_id }}</p>
+
+            <p>Client ID: {{ item.Clients_id }}</p>
+
+            <p>Building Address: {{ fullBuildingAddress }}</p>
+
+            <p
+              v-if="
+                item.Building_description &&
+                item.Building_description.trim() !== ''
+              "
+            >
+              Description: {{ item.Building_description }}
+            </p>
+            <q-card-actions align="right">
+              <q-btn label="Close" color="primary" @click="closeDialogBox" />
+            </q-card-actions>
+          </template>
+
+          <template v-else>
+            <q-input
+              v-model="editedItem.Building_name"
+              label="Building Name"
+              :rules="[(val) => val.length > 0 || 'Cannot be empty']"
+              hide-bottom-space
             />
-          </div>
-  
-      <p >
-        Building ID: {{ item.Buildings_id  }}
-      </p>
-     
-      <p >
-        Client ID: {{ item.Clients_id }}
-      </p>
 
-      <p >
-        Building Address: {{ fullBuildingAddress }}
-      </p>
+            <q-input v-model="editedItem.Building_street" label="Street Name" />
+            <q-input
+              v-model="editedItem.Building_streetNumber"
+              label="House Number"
+            />
+            <q-input v-model="editedItem.Building_zipCode" label="Zipcode" />
+            <q-input v-model="editedItem.Building_city" label="City" />
+            <q-input
+              v-model="editedItem.Building_description"
+              label="Description"
+            />
 
-      <p v-if="item.Building_description && item.Building_description.trim() !== ''">
-        Description: {{ item.Building_description }}
-      </p>
-      <q-card-actions align="right">
-        <q-btn label="Close" color="primary" @click=closeDialogBox />
-      </q-card-actions> 
-    </template>
-    
-    <template v-else>       
-      <q-input
-        v-model="editedItem.Building_name"
-        label="Building Name"
-        :rules="[val => val.length > 0 || 'Cannot be empty']"
-        hide-bottom-space
-        />
-          
-      <q-input v-model="editedItem.Building_street" label="Street Name" />
-      <q-input v-model="editedItem.Building_streetNumber" label="House Number" />
-      <q-input v-model="editedItem.Building_zipCode" label="Zipcode" />
-      <q-input v-model="editedItem.Building_city" label="City" />
-      <q-input v-model="editedItem.Building_description" label="Description" />
-
-      <q-card-actions align="right">
-        <q-btn  label="Save" color="primary" @click="update_building()" />
-        <q-btn  label="Exit" color="primary" @click= "editing = false" />
-        <q-btn  label="Delete" color=red @click= deleteBuildingData />
-      </q-card-actions>
-    </template>
-
-    </q-card-section>
-                      
-    </q-card>
-  </q-dialog>        
+            <q-card-actions align="right">
+              <q-btn label="Save" color="primary" @click="update_building()" />
+              <q-btn label="Exit" color="primary" @click="editing = false" />
+              <q-btn label="Delete" color="red" @click="deleteBuildingData" />
+            </q-card-actions>
+          </template>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </div>
- </template>
-     
-   <script>
-     import { ref } from 'vue';
-     import { supabase } from '../assets/supabase';
+</template>
 
-     export default {
-       name: 'BuildingCardItem',
-       props: ['item'],
- 
-       methods:{
-         OpenDialogBox(){
-           
-           console.log('REFRESGIN')
-         },
+<script>
+import { ref } from 'vue';
+import { supabase } from '../assets/supabase';
 
-         closeDialogBox(){
-          this.editing = false;
-          this.infoDialog = false;
-         },
+export default {
+  name: 'BuildingCardItem',
+  props: ['item'],
 
-         startEditing() {
-        // Start editing by copying the original item
-          this.editedItem = { ...this.item };
-          this.editing = true;
-        },
+  methods: {
+    OpenDialogBox() {
+      console.log('REFRESGIN');
+    },
 
-        async update_building(){
-          let Building_id = this.props.item.Buildings_id
-      
-        
-          const { data, error } = await supabase
-              .from('Buildings')
-              .update({ Building_name: this.editedItem.Building_name, 
-                Building_street : this.editedItem.Building_street, 
-                Building_streetNumber: this.editedItem.Building_streetNumber, 
-                Building_zipCode : this.editedItem.Building_zipCode,
-                Building_city : this.editedItem.Building_city,
-                Building_description :this.editedItem.Building_description,
-               })
-              .eq('Buildings_id', Building_id)
-              .select()
+    closeDialogBox() {
+      this.editing = false;
+      this.infoDialog = false;
+    },
 
-              this.editing = false;
+    startEditing() {
+      // Start editing by copying the original item
+      this.editedItem = { ...this.item };
+      this.editing = true;
+    },
 
-        },
+    async update_building() {
+      let Building_id = this.props.item.Buildings_id;
 
-        async deleteBuildingData() {
-          let Building_id = this.props.item.Buildings_id
+      const { data, error } = await supabase
+        .from('Buildings')
+        .update({
+          Building_name: this.editedItem.Building_name,
+          Building_street: this.editedItem.Building_street,
+          Building_streetNumber: this.editedItem.Building_streetNumber,
+          Building_zipCode: this.editedItem.Building_zipCode,
+          Building_city: this.editedItem.Building_city,
+          Building_description: this.editedItem.Building_description,
+        })
+        .eq('Buildings_id', Building_id)
+        .select();
 
-          
-          const { error } = await supabase
-            .from('Buildings')
-            .delete()
-            .eq('Buildings_id', Building_id)
+      this.editing = false;
+    },
 
-            this.editing = false;
-            this.infoDialog = false
-        }
+    async deleteBuildingData() {
+      let Building_id = this.props.item.Buildings_id;
 
-         
-     },
- 
-         setup(props) {
-            const fullBuildingAddress = props.item.Building_street + " " + props.item.Building_streetNumber + ", " +  props.item.Building_zipCode + ", " + props.item.Building_city
-            const infoDialog = ref(false);
-            const editing = ref(false);
-            const editedItem = ref([]);
-             
-                 
- 
-             return {
-              props,
-              infoDialog,
-              fullBuildingAddress,
-              editing,
-              editedItem,
- 
-         };
-         }
- 
-     
-     };
-   </script>
-     
- <!-- 
-     ? 
+      const { error } = await supabase
+        .from('Buildings')
+        .delete()
+        .eq('Buildings_id', Building_id);
+
+      this.editing = false;
+      this.infoDialog = false;
+    },
+  },
+
+  setup(props) {
+    const fullBuildingAddress =
+      props.item.Building_street +
+      ' ' +
+      props.item.Building_streetNumber +
+      ', ' +
+      props.item.Building_zipCode +
+      ', ' +
+      props.item.Building_city;
+    const infoDialog = ref(false);
+    const editing = ref(false);
+    const editedItem = ref([]);
+
+    return {
+      props,
+      infoDialog,
+      fullBuildingAddress,
+      editing,
+      editedItem,
+    };
+  },
+};
+</script>
+
+<!--
+     ?
      // IF CLIENTS SELECTED
      clients.filter((client) => client.Clients_name.toLowerCase().includes(search.toLowerCase()))
 
--- quasar-project/src/components/ClientCardItem.vue
+++ quasar-project/src/components/ClientCardItem.vue
@@ -1,172 +1,160 @@
 <template>
-   <!-- loop to generate all infos, filter by search -->
-   <div class="q-pa-md q-gutter-sm">
-   <button
-          class="border border-lnf-navy rounded-xl px-5 py-4 hover:bg-lnf-navy/5 duration-100 text-left"
-    
-          @click="infoDialog = true"
-        >
-          <p>
-            {{ item.Clients_name }}
-          </p>
-          
-  </button>
+  <!-- loop to generate all infos, filter by search -->
+  <div>
+    <button
+      class="bg-lnf-navy/5 rounded-xl px-5 py-4 text-left w-full"
+      @click="infoDialog = true"
+    >
+      <p>
+        {{ item.Clients_name }}
+      </p>
+    </button>
 
-
-  <q-dialog v-model="infoDialog" @hide="editing=false">
-    <q-card>
-      <q-card-section class="q-pa-md">
-
-        <template v-if="!editing">
-          <div class="row justify-between">
-            <h4>
-              <span >{{ editedItem.Clients_name}}</span>
-            </h4>
-            <q-btn
-              v-if="!editing"
-              @click="startEditing"
-              icon="edit"
-            />
-          </div>
+    <q-dialog v-model="infoDialog" @hide="editing = false">
+      <q-card>
+        <q-card-section class="q-pa-md">
+          <template v-if="!editing">
+            <div class="row justify-between">
+              <h4>
+                <span>{{ editedItem.Clients_name }}</span>
+              </h4>
+              <q-btn v-if="!editing" @click="startEditing" icon="edit" />
+            </div>
             <p>Client ID: {{ item.Clients_id }}</p>
-            <p>Phone Number:  {{item.Phone_number }}</p>
+            <p>Phone Number: {{ item.Phone_number }}</p>
             <p>Address: {{ fullAddress }}</p>
-            
+
             <!-- Buildings button in client information-->
             <q-card-actions class="q-pa-md">
-              <q-btn label="Buildings" @click="go_to_client_buildings()" style="text-transform: none;" />
+              <q-btn
+                label="Buildings"
+                @click="go_to_client_buildings()"
+                style="text-transform: none"
+              />
             </q-card-actions>
 
-      <q-card-actions align="right">
-        <q-btn label="Close" color="primary" @click="infoDialog = false" />
-      </q-card-actions>
-
+            <q-card-actions align="right">
+              <q-btn
+                label="Close"
+                color="primary"
+                @click="infoDialog = false"
+              />
+            </q-card-actions>
           </template>
 
           <template v-else>
-            
-              <q-input
-                v-model="editedItem.Clients_name"
-                label="Client Name"
-                :rules="[val => val.length > 0 || 'Cannot be empty']"
-                hide-bottom-space
-              />
-            
+            <q-input
+              v-model="editedItem.Clients_name"
+              label="Client Name"
+              :rules="[(val) => val.length > 0 || 'Cannot be empty']"
+              hide-bottom-space
+            />
+
             <q-input v-model="editedItem.Phone_number" label="Phone number" />
-            <q-input v-model="editedItem.Address_StreetName" label="Street Name" />
-            <q-input v-model="editedItem.Address_HouseNumber" label="House Number" />
+            <q-input
+              v-model="editedItem.Address_StreetName"
+              label="Street Name"
+            />
+            <q-input
+              v-model="editedItem.Address_HouseNumber"
+              label="House Number"
+            />
             <q-input v-model="editedItem.Address_Zipcode" label="Zipcode" />
             <q-input v-model="editedItem.Address_City" label="City" />
 
             <q-card-actions align="right">
-        <q-btn  label="Save" color="primary" @click="update_client()" />
-        <q-btn  label="Exit" color="primary" @click= "editing = false" />
-        <q-btn  label="Delete" color=red @click= deleteClientData() />
-          </q-card-actions>
+              <q-btn label="Save" color="primary" @click="update_client()" />
+              <q-btn label="Exit" color="primary" @click="editing = false" />
+              <q-btn label="Delete" color="red" @click="deleteClientData()" />
+            </q-card-actions>
           </template>
-
-         
-      </q-card-section>
-
-     
-      
-      
-    </q-card>
-  </q-dialog>
-</div>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </div>
 </template>
-         
-         
-    
-  <script>
-    import { ref } from 'vue';
-    import { supabase } from '../assets/supabase';
 
-    export default {
-      name: 'ClientCardItem',
-      props: ['item'],
+<script>
+import { ref } from 'vue';
+import { supabase } from '../assets/supabase';
 
-      methods:{
-        go_to_client_buildings(){
-          this.$emit('exit_card', this.props.item);
-          console.log('REFRESGIN')
-        },
+export default {
+  name: 'ClientCardItem',
+  props: ['item'],
 
-        startEditing() {
-        // Start editing by copying the original item
-         
-          this.editing = true;
-        },
-
-        async update_client(){
-          let client_id = this.props.item.Clients_id
-      
-        
-          const { data, error } = await supabase
-              .from('Clients')
-              .update({ Clients_name: this.editedItem.Clients_name, 
-                Phone_number : this.editedItem.Phone_number, 
-                Address_HouseNumber: this.editedItem.Address_HouseNumber, 
-                Address_StreetName : this.editedItem.Address_StreetName,
-                Address_Zipcode : this.editedItem.Address_Zipcode,
-                Address_City :this.editedItem.Address_City,
-               })
-              .eq('Clients_id', client_id)
-              .select()
-
-
-              this.refreshPage();
-              this.editing = false;
-
-        },
-
-        async deleteClientData() {
-          let client_id = this.props.item.Clients_id
-
-          const { error } = await supabase
-            .from('Clients')
-            .delete()
-            .eq('Clients_id', client_id)
-
-            this.editing = false;
-            this.infoDialog = false
-        },
-        refreshPage(){
-          // location.reload();
-          
-        },
-
-
-
+  methods: {
+    go_to_client_buildings() {
+      this.$emit('exit_card', this.props.item);
+      console.log('REFRESGIN');
     },
-  
 
-        setup(props) {
-          const fullAddress = props.item.Address_StreetName + ' ' + props.item.Address_HouseNumber + ', '  +  props.item.Address_Zipcode + ', '+ props.item.Address_City 
-          const infoDialog = ref(false);
-          const editing = ref(false);
-          const editedItem = ref([]);
+    startEditing() {
+      // Start editing by copying the original item
 
-          editedItem.value = { ...props.item };
-         
-          
+      this.editing = true;
+    },
 
-          return {
-            props,
-            infoDialog,
-            fullAddress,
+    async update_client() {
+      let client_id = this.props.item.Clients_id;
 
-            editing,
-            editedItem,
-                  
-        };
-      }
+      const { data, error } = await supabase
+        .from('Clients')
+        .update({
+          Clients_name: this.editedItem.Clients_name,
+          Phone_number: this.editedItem.Phone_number,
+          Address_HouseNumber: this.editedItem.Address_HouseNumber,
+          Address_StreetName: this.editedItem.Address_StreetName,
+          Address_Zipcode: this.editedItem.Address_Zipcode,
+          Address_City: this.editedItem.Address_City,
+        })
+        .eq('Clients_id', client_id)
+        .select();
+
+      this.refreshPage();
+      this.editing = false;
+    },
+
+    async deleteClientData() {
+      let client_id = this.props.item.Clients_id;
+
+      const { error } = await supabase
+        .from('Clients')
+        .delete()
+        .eq('Clients_id', client_id);
+
+      this.editing = false;
+      this.infoDialog = false;
+    },
+    refreshPage() {
+      // location.reload();
+    },
+  },
+
+  setup(props) {
+    const fullAddress =
+      props.item.Address_StreetName +
+      ' ' +
+      props.item.Address_HouseNumber +
+      ', ' +
+      props.item.Address_Zipcode +
+      ', ' +
+      props.item.Address_City;
+    const infoDialog = ref(false);
+    const editing = ref(false);
+    const editedItem = ref([]);
+
+    editedItem.value = { ...props.item };
+
+    return {
+      props,
+      infoDialog,
+      fullAddress,
+
+      editing,
+      editedItem,
     };
-
-    
-    
-  
-
+  },
+};
 </script>
 
 <!--
